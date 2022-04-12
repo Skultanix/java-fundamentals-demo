48c6503a49fb990562f488e6d7e9a7165d6de425
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-04-12 17:32:57 -0600

    Created Droid class.

---
+++ Droid-Factory/Droid.java
@@ -0,0 +1,10 @@
+public class Droid {
+    private String serialNumber;
+    private Model model;
+
+    public Droid(String serialNumber, Model model){
+        this.serialNumber = serialNumber;
+        this.model = model;
+    }
+
+}
